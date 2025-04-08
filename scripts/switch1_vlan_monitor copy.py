0d4f74d9c2aeb5d6efd2138fba56bc21819675f6
EliasDeHondt <Elias.dehondt@outlook.com>
2025-04-08 12:49:17 +0100

    Add initial implementation of switch1_vlan_monitor script with basic structure and metadata

---
+++ scripts/switch1_vlan_monitor copy.py
@@ -0,0 +1,20 @@
+############################
+# @author Elias De Hondt   #
+# @since 07/04/2025        #
+############################
+# type: ignore[annotation-unchecked]
+
+import math
+from json import dumps
+
+Manifest = {
+    'Name': 'switch1_vlan_monitor',
+    'Description': '',
+    'Version': '1.0',
+    'Tags': ['', ''],
+    'Author': ''
+}
+
+ParameterDefinitions = {
+
+}
