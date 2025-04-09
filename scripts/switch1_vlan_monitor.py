5375ef754fa2c18122e87cb8d80e4284f62fba7a
Kobe Wijnants <wijnantskobe28@gmail.com>
2025-04-09 10:51:01 +0100

    remove dropped packets from interface monitor (see them on copp)

--- scripts/switch1_vlan_monitor.py
+++ scripts/switch1_vlan_monitor.py
@@ -2,7 +2,7 @@ Manifest = {
     "Name": "switch1_interface_monitor",
     "Description": "Physical Interface Tx/Rx statistics monitoring agent"
     " using multi-graph",
-    "Version": "1.2",
+    "Version": "1.0",
     "TargetSoftwareVersion": "10.11",
     "Author": "Kobe Wijnants",
 }
@@ -40,30 +40,44 @@ class Agent(NAE):
         self.r1.action("ALERT_LEVEL", AlertLevel.CRITICAL)
         self.r1.clear_action("ALERT_LEVEL", AlertLevel.NONE)
 
-        # tx packets
+        # rx packets dropped
         uri2 = (
+            "/rest/v1/system/interfaces/*?attributes=statistics.rx_dropped"
+            + "&filter=type:system"
+        )
+        self.m2 = Monitor(uri2, "Rx Packets Dropped (packets)")
+
+        # tx packets
+        uri3 = (
             "/rest/v1/system/interfaces/*?attributes=statistics.tx_packets"
             + "&filter=type:system"
         )
-        rate_m2 = Rate(uri2, "20 seconds")
-        self.m2 = Monitor(rate_m2, "Tx Packets (packets per second)")
-        self.r2 = Rule("Rule to Monitor Interface tx Packets")
+        rate_m3 = Rate(uri3, "20 seconds")
+        self.m3 = Monitor(rate_m3, "Tx Packets (packets per second)")
+        self.r3 = Rule("Rule to Monitor Interface tx Packets")
 
-        self.r2.condition("{} > {}", [self.m2, self.params["tx_packets_threshold"]])
-        self.r2.clear_condition(
-            "{} < {}", [self.m2, self.params["tx_packets_threshold"]]
+        self.r3.condition("{} > {}", [self.m3, self.params["tx_packets_threshold"]])
+        self.r3.clear_condition(
+            "{} < {}", [self.m3, self.params["tx_packets_threshold"]]
         )
-        self.r2.action("ALERT_LEVEL", AlertLevel.CRITICAL)
-        self.r2.clear_action("ALERT_LEVEL", AlertLevel.NONE)
+        self.r3.action("ALERT_LEVEL", AlertLevel.CRITICAL)
+        self.r3.clear_action("ALERT_LEVEL", AlertLevel.NONE)
+
+        # tx packets dropped
+        uri4 = (
+            "/rest/v1/system/interfaces/*?attributes=statistics.tx_dropped"
+            + "&filter=type:system"
+        )
+        self.m4 = Monitor(uri4, "Tx Packets Dropped (packets)")
 
         # graph display for change of traffic and packets drop
-        # self.graph_rx_packets = Graph(
-        #     [self.m1],
-        #     title=Title("Rate of change of Interface Rx packets (in seconds)"),
-        #     dashboard_display=True,
-        # )
-        # self.graph_tx_packets = Graph(
-        #     [self.m2],
-        #     title=Title("Rate of change of Interface Tx packets (in seconds)"),
-        #     dashboard_display=False,
-        # )
+        self.graph_tx_rx_packets = Graph(
+            [self.m1, self.m3],
+            title=Title("Rate of change of Interface Tx/Rx packets (in seconds)"),
+            dashboard_display=True,
+        )
+        self.graph_tx_rx_drop = Graph(
+            [self.m2, self.m4],
+            title=Title("Interface Tx/Rx packets drop count"),
+            dashboard_display=False,
+        )
